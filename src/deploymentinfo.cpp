bfc0df4ff89beb3c4dd149efd16874e087aa58bb
Mytherra Blockchain <panto@mytherrablockchain.org>
2025-10-26 08:46:11 +0100

    Add checkpoints and fix versionbit warnings
    Versionbit deployments 14, 23, 26 configured

--- src/deploymentinfo.cpp
+++ src/deploymentinfo.cpp
@@ -17,6 +17,18 @@ const struct VBDeploymentInfo VersionBitsDeploymentInfo[Consensus::MAX_VERSION_B
         /*.name =*/ "taproot",
         /*.gbt_force =*/ true,
     },
+    {
+        /*.name =*/ "unknown14",
+        /*.gbt_force =*/ true,
+    },
+    {
+        /*.name =*/ "unknown23",
+        /*.gbt_force =*/ true,
+    },
+    {
+        /*.name =*/ "unknown26",
+        /*.gbt_force =*/ true,
+    },
 };
 
 std::string DeploymentName(Consensus::BuriedDeployment dep)
